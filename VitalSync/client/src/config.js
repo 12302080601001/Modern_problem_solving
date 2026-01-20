bb6c9ed14860e3ae02cd31e723841320ba819efc
12302080601001 <12302080601001@adit.ac.in>
2026-01-20 20:59:58 +0530

    Update config.js

--- VitalSync/client/src/config.js
+++ VitalSync/client/src/config.js
@@ -5,5 +5,5 @@ const isLocal = window.location.hostname === "localhost";
 export const API_URL = isLocal
   ? "http://localhost:3001"                  // If you are on your laptop
   : "https://vitalsync-syuf.onrender.com/"; // If you are on the internet
-
+export default API_URL;
 // ⚠️ IMPORTANT: If you know your actual Render Backend URL, replace the link above!
